ca5716afd4b4b8fa71841a731dfb457e36ee6431
amekusa <hello@amekusa.com>
2024-01-17 12:13:17 +0900

    More informative outputs

--- src/test.js
+++ src/test.js
@@ -10,15 +10,29 @@ const test = {
 	/**
 	 * @param {function} fn
 	 * @param {Array|object} tests
-	 * @param {string} [assertFn]
+	 * @param {string|function} [assertFn]
 	 */
 	testFn(fn, tests, assertFn = 'equal') {
-		if (!(assertFn in assert)) throw `no such method in assert as '${assertFn}'`;
+		if (typeof assertFn == 'string') {
+			if (!(assertFn in assert)) throw `no such method in assert as '${assertFn}'`;
+			assertFn = assert[assertFn];
+		}
 		describe(fn.displayName || fn.name, () => {
-			for (let i in tests) {
-				it(`${i}`, () => {
-					assert[assertFn](fn(...tests[i][0]), tests[i][1]);
-				});
+			if (Array.isArray(tests)) {
+				for (let i = 0; i < tests.length; i++) {
+					let t = tests[i];
+					it(`#${i} ${t[0].join(', ')}`, () => {
+						assertFn(fn(...t[0]), t[1]);
+					});
+				}
+			} else {
+				let keys = Object.keys(tests);
+				for (let i = 0; i < keys.length; i++) {
+					let t = tests[keys[i]];
+					it(`#${i} ${keys[i]}`, () => {
+						assertFn(fn(...t[0]), t[1]);
+					});
+				}
 			}
 		});
 	},
